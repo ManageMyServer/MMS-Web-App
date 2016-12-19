12ba6255a5fd58cada67601aa2bba5658f91f3e7
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-19 18:14:08 -0500

    Update install.php

--- install.php
+++ install.php
@@ -1,5 +1,14 @@
 <!-- (c) ManageMyServer -->
 <head>
   <link href="/core/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="/core/assets/js/jquery-3.1.1.min.js"></script>
+  <script type="text/javascript" src="/core/assets/js/bootstrap.min.js"></script>
   <title>Install - ManageMyServer</title>
 </head>
+<body>
+  <center><h2>Install ManageMyServer</h2></center>
+  <!-- Insert installer here -->
+  <footer>
+    <br><hr><p class="pull-right">This installer was made by ManageMyServer.</p><a href="https://github.com/managemyserver/">&copy; ManageMyServer</a>
+  </footer>
+</body>
