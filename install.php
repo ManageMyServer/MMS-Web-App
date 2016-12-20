bd3cc0f9231313931262be83ef0ba041689e9a66
Joshua Radich <joshuaradich@gmail.com>
2016-12-19 23:39:19 -0500

    shhhh

--- install.php
+++ install.php
@@ -9,7 +9,7 @@
     <div class="container">
    <center><h2>Install ManageMyServer</h2></center>
    <!-- Insert installer here Tables: users nodes servers settings -->
-     <?php
+        <?php
    if ($_GET["page"] == "") {
    echo "
       <title>Welcome - Install - ManageMyServer</title>
@@ -45,7 +45,7 @@
       </div>
       <br><input class=\"btn btn-success btn-medium\" type=\"submit\" name=\"submit\" value=\"Validate &amp; Continue\"></input></p>";
    } elseif ($_GET["page"] == "validate") {
-   
+
    echo "
       <title>Validate - Install - ManageMyServer</title>
       <h2>Validate Database</h2>
@@ -56,13 +56,13 @@
    echo $_GET['db_username'];
    echo $_GET['db_password'];
    echo $_GET['db_prefix'];*/
-   
+
    $address = $_POST['db_address'];
    $username = $_POST['db_username'];
    $password = $_POST['db_password'];
    $database = $_POST['db_name'];
    $prefix = $_POST['db_prefix'];
-   
+
    // Create connection
    $conn = new mysqli($address, $username, $password, $database);
 
@@ -70,10 +70,11 @@
    if ($conn->connect_error) {
        echo("Connection failed: " . $conn->connect_error. "</b><br><br><a href=\"#\" class=\"btn btn-success btn-medium\" onclick=\"window.history.back()\">Go Back</a>");
    } else {
-      
+
          echo "Connected successfully to the database.</b><br><br><a href=\"install.php?page=setup_db\" class=\"btn btn-success btn-medium\">Continue</a>";
          file_put_contents('core/config.php', '
 <?php
+return [
    $db_address = \''.$address.'\';
    $db_name = \''.$database.'\';
    $db_username = \''.$username.'\';
@@ -81,7 +82,7 @@
    $table_prefix = \''.$prefix.'\';
 ?>
 ');
-      
+
    } echo "</p>";
    //End Vertify
    } elseif ($_GET["page"] == "setup_db") {
@@ -91,7 +92,7 @@
       <p>The installer will now setup the database for use with ManageMyServer.</p>
    ";
    include 'core/config.php';
-   
+
    //START TABLE SETUP
    // Create connection
    $conn = new mysqli($db_address, $db_username, $db_password, $db_name);
@@ -99,32 +100,32 @@
    if ($conn->connect_error) {
       die('Connection failed: ' . $conn->connect_error);
    }
-   
+
    //Create tables
    $sql = 'CREATE TABLE '. $table_prefix .'_prefs (
-   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR(30) NOT NULL,
    value VARCHAR(30) NOT NULL
    )';
-   
+
    if ($conn->query($sql) === TRUE) {
       echo 'Table '. $table_prefix. '_prefs created successfully.';
    } else {
       echo 'Error creating table: ' . $conn->error;
    }echo '<br>';
-   
+
    $sql = 'CREATE TABLE '. $table_prefix .'_users (
-   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    username VARCHAR(30) NOT NULL,
    password VARCHAR(30) NOT NULL
    )';
-   
+
    if ($conn->query($sql) === TRUE) {
       echo 'Table '. $table_prefix. '_users created successfully.';
    } else {
       echo 'Error creating table: ' . $conn->error;
    }echo '<br>';
-   
+
    //Create Record
    $sql = 'INSERT INTO '. $table_prefix. '_users (username, password)
            VALUES (\'Admin\', \'password\')';
@@ -134,7 +135,7 @@
    } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
    }echo '<br>';
-   
+
    //Create Record
    $sql = 'INSERT INTO '. $table_prefix. '_prefs (name, value)
            VALUES (\'sitename\', \'Example Site\')';
@@ -144,7 +145,7 @@
    } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
    }echo '<br>';
-   
+
    //END SETUP TABLE
    echo '<br><a href="admin.php" class="btn btn-success btn-medium">Continue</a>';
 
@@ -154,15 +155,15 @@ $conn->close();
       <title>404 - Install - ManageMyServer</title>
       <h2>404 - Uh-oh!</h2>
       <p>Looks like the page you requested isn't at this location!</p>
-      
+
       <div class=\"btn-group\" role=\"group\" aria-label=\"...\">
          <a href=\"#\" class=\"btn btn-success btn-medium\" onclick=\"window.history.back()\">Back</a>
          <a href=\"/\" class=\"btn btn-primary btn-medium\">Home</a>
       </div>
-      
+
       ";
    }
-?>
+        ?>
    <footer><br><hr><p class="pull-right">This installer was made by ManageMyServer.</p><a href="https://github.com/managemyserver/">&copy; ManageMyServer <?php echo date("Y"); ?></a>
   </footer>
   </div>
