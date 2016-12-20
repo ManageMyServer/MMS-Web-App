b2cd7bea542055853f57ef2e00fc9b7e752fbc46
Joshua Radich <joshuaradich@gmail.com>
2016-12-19 22:57:06 -0500

    Commit

--- install.php
+++ install.php
@@ -1,21 +1,23 @@
 <!-- (c) ManageMyServer <?php echo date("Y"); ?> -->
 <head>
-  <link href="/core/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <script type="text/javascript" src="/core/assets/js/jquery-3.1.1.min.js"></script>
-  <script type="text/javascript" src="/core/assets/js/bootstrap.min.js"></script>
-  <title>Install - ManageMyServer</title>
+    <link href="/core/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="/core/assets/js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="/core/assets/js/bootstrap.min.js"></script>
+    <title>Install - ManageMyServer</title>
 </head>
 <body>
-   <center><h2>Install ManageMyServer</h2></center>
-   <!-- Insert installer here Tables: users nodes servers settings -->
-   <?php
-   if ($_GET["page"] == "") {
-   echo "
+    <center>
+        <h2>Install ManageMyServer</h2>
+    </center>
+    <!-- Insert installer here Tables: users nodes servers settings -->
+    <?php
+    if ($_GET["page"] == "") {
+        echo "
        <h2>Install</h2>
-       <p>Welcome to the ManagsMyServer installer. Here is where you can intall ManageMyServer on your computer. You must have MySQL and PHP for this to work. Thanks for using ManageMyServer.
+       <p>Welcome to the ManagsMyServer installer. Here is where you can intall ManageMyServer on your computer. You must have MySQL and PHP for this to work. Thanks for using ManageMyServer.
        <br><br><a href=\"install.php?page=database\" class=\"btn btn-success btn-medium\">Continue</a></p>";
-   } elseif ($_GET["page"] == "database") {
-   echo "
+    } elseif ($_GET["page"] == "database") {
+        echo "
        <h2>Configure Database</h2>
        <p>
        This will lead you through the database setup.
@@ -41,24 +43,24 @@
            <input type=\"text\" name=\"db_prefix\" class=\"form-control\" id=\"inputDefault\">
        </div>
        <br><input class=\"btn btn-success btn-medium\" type=\"submit\" name=\"submit\" value=\"Validate &amp; Continue\"></input></p>";
-   } elseif ($_GET["page"] == "validate") {
-   echo "
+    } elseif ($_GET["page"] == "validate") {
+        echo "
        <h2>Validate Database</h2>
        <p>
        This will validate the information you entered on the previous page.<br><b>";
-       $address = $_POST['db_address'];
-       $username = $_POST['db_username'];
-       $password = $_POST['db_password'];
-       $database = $_POST['db_name'];
-       $prefix = $_POST['db_prefix'];
-       // Create connection
-       $conn = new mysqli($address, $username, $password, $database);
-       // Check connection
-       if ($conn->connect_error) {
-           echo("Connection failed: " . $conn->connect_error. "</b><br><br><a href=\"#\" class=\"btn btn-success btn-medium\" onclick=\"window.history.back()\">Go Back</a>");
-       } else {
-       echo "Connected successfully to the database.</b><br><br><a href=\"install.php?page=setup_db\" class=\"btn btn-success btn-medium\">Continue</a>";
-       file_put_contents('core/config.php', '
+        $address = $_POST['db_address'];
+        $username = $_POST['db_username'];
+        $password = $_POST['db_password'];
+        $database = $_POST['db_name'];
+        $prefix = $_POST['db_prefix'];
+        // Create connection
+        $conn = new mysqli($address, $username, $password, $database);
+        // Check connection
+        if ($conn->connect_error) {
+            echo("Connection failed: " . $conn->connect_error. "</b><br><br><a href=\"#\" class=\"btn btn-success btn-medium\" onclick=\"window.history.back()\">Go Back</a>");
+        } else {
+            echo "Connected successfully to the database.</b><br><br><a href=\"install.php?page=setup_db\" class=\"btn btn-success btn-medium\">Continue</a>";
+            file_put_contents('core/config.php', '
 <?php
    $db_address = \''.$address.'\';
    $db_name = \''.$database.'\';
@@ -67,72 +69,76 @@
    $table_prefix = \''.$prefix.'\';
 ?>
 ');
-      } echo "</p>";
-      //End Vertify
-      } elseif ($_GET["page"] == "setup_db") {
-      echo "
+        } echo "</p>";
+        //End Vertify
+    } elseif ($_GET["page"] == "setup_db") {
+        echo "
       <h2>Setup Database</h2>
-      <p>The installer will now setup the database for use with ManageMyServer.</p>
-      ";
-      include 'core/config.php';
-   
-      //START TABLE SETUP
-      // Create connection
-      $conn = new mysqli($db_address, $db_username, $db_password, $db_name);
-      // Check connection
-      if ($conn->connect_error) {
-          die('Connection failed: ' . $conn->connect_error);
-      }
-   
-   //Create tables
-   $sql = 'CREATE TABLE '. $table_prefix .'_prefs (
-   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+      <p>The installer will now setup the database for use with ManageMyServer.</p>
+      ";
+        include 'core/config.php';
+
+        //START TABLE SETUP
+        // Create connection
+        $conn = new mysqli($db_address, $db_username, $db_password, $db_name);
+        // Check connection
+        if ($conn->connect_error) {
+            die('Connection failed: ' . $conn->connect_error);
+        }
+
+        //Create tables
+        $sql = 'CREATE TABLE '. $table_prefix .'_prefs (
+   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR(30) NOT NULL,
    value VARCHAR(30) NOT NULL
    )';
-   
-   if ($conn->query($sql) === TRUE) {
-      echo 'Table '. $table_prefix. '_prefs created successfully.';
-   } else {
-      echo 'Error creating table: ' . $conn->error;
-   }echo '<br>';
-   
-   $sql = 'CREATE TABLE '. $table_prefix .'_users (
-   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+
+        if ($conn->query($sql) === TRUE) {
+            echo 'Table '. $table_prefix. '_prefs created successfully.';
+        } else {
+            echo 'Error creating table: ' . $conn->error;
+        }echo '<br>';
+
+        $sql = 'CREATE TABLE '. $table_prefix .'_users (
+   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    username VARCHAR(30) NOT NULL,
    password VARCHAR(30) NOT NULL
    )';
-   
-   if ($conn->query($sql) === TRUE) {
-      echo 'Table '. $table_prefix. '_users created successfully.';
-   } else {
-      echo 'Error creating table: ' . $conn->error;
-   }echo '<br>';
-   
-   //Create Record
-   $sql = 'INSERT INTO '. $table_prefix. '_users (username, password)
+
+        if ($conn->query($sql) === TRUE) {
+            echo 'Table '. $table_prefix. '_users created successfully.';
+        } else {
+            echo 'Error creating table: ' . $conn->error;
+        }echo '<br>';
+
+        //Create Record
+        $sql = 'INSERT INTO '. $table_prefix. '_users (username, password)
            VALUES (\'Admin\', \'password\')';
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully.";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }echo '<br>';
 
-   if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully.";
-   } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-   }echo '<br>';
-   
-   //Create Record
-   $sql = 'INSERT INTO '. $table_prefix. '_prefs (name, value)
+        //Create Record
+        $sql = 'INSERT INTO '. $table_prefix. '_prefs (name, value)
            VALUES (\'sitename\', \'Example Site\')';
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully.";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }echo '<br>';
 
-   if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully.";
-   } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-   }echo '<br>';
-   
-   //END SETUP TABLE
-   echo '<br><a href="admin.php" class="btn btn-success btn-medium">Continue</a>';
-
-$conn->close();  ?><footer>
-    <br><hr><p class="pull-right">This installer was made by ManageMyServer.</p><a href="https://github.com/managemyserver/">&copy; ManageMyServer <?php echo date("Y"); ?></a>
-  </footer>
+        //END SETUP TABLE
+        echo '<br><a href="admin.php" class="btn btn-success btn-medium">Continue</a>';
+        $conn->close();
+    }; ?>
+    <footer>
+        <br />
+        <hr />
+        <p class="pull-right">This installer was made by ManageMyServer.</p>
+        <a href="https://github.com/managemyserver/">
+            &copy; ManageMyServer <?php echo date("Y"); ?>
+        </a>
+    </footer>
 </body>
