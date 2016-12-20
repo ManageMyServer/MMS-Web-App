b06ab5e479cb311484fb87f31efd4596599ebf75
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-19 23:14:06 -0500

    Better 404 Page

--- pages/errors/404.php
+++ pages/errors/404.php
@@ -1,3 +1,6 @@
-<?php
-echo "404 error";
-?>
+<h2>404</h2>
+<h5>Looks like you made it to an unknown location!</h5>
+<div class="btn-group" role="group" aria-label="...">
+    <a href="#" class="btn btn-success btn-medium" onclick="window.history.back()">Back</a>
+    <a href="/" class="btn btn-primary btn-medium">Home</a>
+</div>
