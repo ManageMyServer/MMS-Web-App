85870500a8e1fd39db9452a5fbc66347f8cfecbe
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-21 13:36:10 -0500

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -2,4 +2,4 @@
     echo 'Hello '.$_SESSION['username'];
 ?>
 , welcome to ManageMyServer.<hr>
-<?php echo file_get_contents('https://managemyserver.github.io/display.html');
+<?php echo file_get_contents('https://managemyserver.github.io/display.html'); ?>
