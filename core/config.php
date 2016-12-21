ddc6aa6c86a9f3f4085f94998adde256d9b154c8
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-21 18:08:16 -0500

    Update config.php

--- core/config.php
+++ core/config.php
@@ -1,11 +1,1 @@
 
-<?php
-return [
-    'db_address' => '127.0.0.1',
-    'db_name' => 'test2',
-    'db_username' => 'root',
-    'db_password' => 'password',
-    'table_prefix' => 'a',
-    'site_title' => 'Site'
-]
-?>
