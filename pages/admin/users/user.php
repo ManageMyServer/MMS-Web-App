9474ffafac31d02884c9a33a4f7816cb13bfa6ca
BrightSkyz <BrightSkyz@users.noreply.github.com>
2016-12-24 23:25:52 -0500

    Update user.php

--- pages/admin/users/user.php
+++ pages/admin/users/user.php
@@ -1,4 +1,53 @@
-ca55fc1b7330e63c8e07419316e41c0e27c3d18c
+<?php
+$id = $_GET['id'];
+$conn = new mysqli($config['db_address'], $config['db_username'], $config['db_password'], $config['db_name']);
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+$sql = 'SELECT * FROM '.$config['table_prefix'].'_users WHERE id=? ORDER BY id';
+$stmt = $conn->stmt_init();
+if(!$stmt->prepare($sql))
+{
+    print "Failed to prepare statement\n";
+}
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $_GET['id']);
+$stmt->execute();
+$result = $stmt->get_result();
+$users = array();
+while ($row = $result->fetch_array(MYSQLI_NUM))
+{
+    $results[] = $row;
+    $username = $row[0];
+    $rank = $row[2];
+    $root = $row[3];
+    $users[$username] = array($rank, $root);
+}
+if(sizeof($users) == 0){
+    echo '<div class="text-center">No users with that ID were found.</div>';
+} elseif(1==2) { // Changed to hide table -> originally } else {
+    echo'<div class="container">
+        <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Type</th>
+                </tr>
+            </thead>
+            <tbody>';
+    foreach($results as $user) {
+        echo'<tr><td>'.$user[4].'<td><a href="/admin/users/user/?id='.$user[4].'">'.$user[0].'</td><td>'.$user[2].'</td></tr>';
+    }
+    echo'</tbody>
+        </table>
+        </div>
+    </div>';
+}
+if(isset($_POST['Delete'])){
+    $config = include($_SERVER['DOCUMENT_ROOT'].'/core/config.php');
         $hash = password_hash($password, PASSWORD_DEFAULT);
         $conn = new mysqli($config['db_address'], $config['db_username'], $config['db_password'], $config['db_name']);
         // Check connection
