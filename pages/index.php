cb9eec78ab753a8773d6d075bdcb2f80b88c2a98
Joshua Radich <joshuaradich@gmail.com>
2016-12-29 15:50:47 -0500

    Changelog only seeable by admins

--- pages/index.php
+++ pages/index.php
@@ -2,4 +2,6 @@
     echo 'Hello '.$_SESSION['username'];
 ?>
 , welcome to ManageMyServer.<hr>
-<?php echo file_get_contents('https://managemyserver.github.io/display/'); ?>
+<?php if(strtolower($_SESSION['rank']) == strtolower('superuser') | strtolower($_SESSION['rank']) == strtolower('Admin')) {
+          echo file_get_contents('https://managemyserver.github.io/display/');
+}?>
