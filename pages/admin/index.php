17147949dd931e9d67404eaf5f4fb06def7eea5d
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-12 16:02:16 -0500

    Added Update Button

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -1,5 +1,6 @@
 <div class="text-center"><h2>Admin Panel</h2></div>
 <a href="/admin/users">Users</a><br />
+<a href="/admin/update">Update</a><br />
 
 <?php
 $conn = new mysqli($config['db_address'], $config['db_username'], $config['db_password'], $config['db_name']);
