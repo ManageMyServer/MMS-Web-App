e40a5862b68d23c2e008f37fe13f6269863fc50b
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-12 16:03:56 -0500

    Broken, as of now

--- pages/admin/update/index.php
+++ pages/admin/update/index.php
@@ -1,7 +1,12 @@
-<span class="text-center"><h2>Update ManageMyServer</h2></span>
+<span class="text-center"><h2>Update ManageMyServer</h2><small> Work in Progress</small></span>
 <?php
-    if("pre-0"==(file_get_contents("https://managemyserver.github.io/display/latest_version.html")) {
-        echo 'Your version of ManageMyServer is <b>up to date</b>.';
+    $local_version = "0";
+    $version = file_get_contents("https://managemyserver.github.io/display/latest_version.html");
+    echo 'Local Version: ' . $local_version;
+    echo '<br>Latest Version: ' . $version;
+    echo '<br><br>';
+    if(($local_version)!==($version)) {
+        echo 'Your version of ManageMyServer is up to date.';
     } else {
         echo file_get_contents("https://managemyserver.github.io/display/update.html");
     }
