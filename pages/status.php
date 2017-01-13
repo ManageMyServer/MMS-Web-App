be034892c0d5d17c4683c328463453104480885f
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-12 19:34:37 -0500

    Create Status (Make it functional)

---
+++ pages/status.php
@@ -0,0 +1,20 @@
+<span class="text-center"><h2>Status <small>Non-Functional (Work in Progress, Display only)</small></h2></span>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Node Name</td>
+            <td><span class="label label-success">Online</span></td>
+        </tr>
+        <tr>
+            <td>Node Name</td>
+            <td><span class="label label-danger">Offline</span></td>
+        </tr>
+    </tbody>
+</table>
