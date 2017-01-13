4ff46e00291d1eb5cc5098d31f46dc231b65abc8
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-12 20:10:14 -0500

    Added Non-Functional Example

--- pages/nodes.php
+++ pages/nodes.php
@@ -1,3 +1,28 @@
-<?php
-    echo 'Nodes page.'; 
-?>
+<span class="text-center"><h2>Nodes <small>Non-Functional (Work in Progress, Display only)</small></h2></span>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Node Name</th>
+            <th>RAM Usage</th>
+            <th>IP</th>
+            <th>Ports Left</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Node1</td>
+            <td>15.22/31.54</td>
+            <td>192.42.32.232</td>
+            <td>5</td>
+            <td><span class="label label-success">Online</span></td>
+        </tr>
+        <tr>
+            <td>Node2</td>
+            <td>--/--</td>
+            <td>192.78.22.32</td>
+            <td>5</td>
+            <td><span class="label label-danger">Offline</span></td>
+        </tr>
+    </tbody>
+</table>
