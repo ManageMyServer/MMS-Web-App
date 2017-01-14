5a9ad577132709f6a6fe194993cde399cc239b9c
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-13 19:03:13 -0500

    Example has been added

--- pages/servers.php
+++ pages/servers.php
@@ -1,3 +1,40 @@
-<?php
-    echo 'Servers Page';
-?>
+<span class="text-center"><h2>Servers <small>Non-Functional (Work in Progress, Display only)</small></h2></span>
+<a href="/servers/addserver" class="btn btn-sm btn-primary">Add Server</a>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Server Name</th>
+            <th>Player Count</th>
+            <th>IP</th>
+            <th>Port</th>
+            <th>Status</th>
+            <th>Node</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Example Server 1</td>
+            <td>15/32</td>
+            <td>192.42.32.232</td>
+            <td>25565</td>
+            <td><span class="label label-success">Online</span></td>
+            <td>Node1</td>
+        </tr>
+        <tr>
+            <td>Another Example Server</td>
+            <td>--/--</td>
+            <td>192.78.22.32</td>
+            <td>25570</td>
+            <td><span class="label label-danger">Offline</span></td>
+            <td>Node2</td>
+        </tr>
+        <tr>
+            <td>MiniNetwork</td>
+            <td>--/--</td>
+            <td>192.42.32.232</td>
+            <td>25692</td>
+            <td><span class="label label-warning">Suspended</span></td>
+            <td>Node1</td>
+        </tr>
+    </tbody>
+</table>
