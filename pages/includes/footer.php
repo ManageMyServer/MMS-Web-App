1f0449ca6b0ca892ec73571724150e1e8b0c8ece
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-13 21:18:21 -0500

    Cleaned Up

--- pages/includes/footer.php
+++ pages/includes/footer.php
@@ -1,8 +1,7 @@
-<!-- We would like it if you kept the credits at the bottom of the page, in this footer. It isn't intrusive to the website. -->
+<!-- We would like it if you kept the credits at the bottom of the page, in this footer. It is not intrusive to the website. -->
 <footer>
     <hr />
     <span class="pull-right">
         <a href="https://github.com/ManageMyServer">&copy; ManageMyServer <?php echo date("Y");?></a>
-
     </span>
 </footer>
