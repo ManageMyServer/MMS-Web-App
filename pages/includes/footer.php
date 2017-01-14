2362f1ab3582f4aa4ec33eb69e123fa98770926f
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-13 21:25:37 -0500

    Footer Update

--- pages/includes/footer.php
+++ pages/includes/footer.php
@@ -1,7 +1,14 @@
-<!-- We would like it if you kept the credits at the bottom of the page, in this footer. It is not intrusive to the website. -->
+<!-- We would like it if you kept the credits at the bottom of the page, in this footer. It isnt intrusive to the website. -->
 <footer>
     <hr />
     <span class="pull-right">
-        <a href="https://github.com/ManageMyServer">&copy; ManageMyServer <?php echo date("Y");?></a>
+        <div class="dropup">
+            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $config['site_title']?>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <li><a href="https://github.com/ManageMyServer">&copy; ManageMyServer <?php echo date("Y");?></a></li>
+            </ul>
+        </div>
     </span>
 </footer>
