54dc5617dc7c8312889b5bc18455706eeaba68da
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-13 21:07:34 -0500

    Changed to $current_version

--- pages/admin/update.php
+++ pages/admin/update.php
@@ -1,13 +1,13 @@
 <span class="text-center"><h2>Update ManageMyServer</h2></span>
 <?php
-    $current_version = "0";
+    $current_version = "dev-0";
     $data_file = file_get_contents("https://managemyserver.github.io/display/data.html");
     $object = json_decode($data_file);
     $version = $object->version;
     echo 'Current Version: ' . $current_version;
     echo '<br>Latest Version: ' . $version;
     echo '<br><br>';
-    if(($local_version)==($version)) {
+    if($current_version == $version) {
         echo 'Your version of ManageMyServer is up to date.';
     } else {
         echo file_get_contents("https://managemyserver.github.io/display/update.html");
