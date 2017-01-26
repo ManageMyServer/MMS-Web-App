13c344484d36bd55339b86f4e9f65c0e7e64c5ad
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-26 14:09:43 -0500

    Added ?continue=
    Now if you do /login?continue=page/url, it redirects you to the page/url after sign-in or while signed in.

--- pages/login.php
+++ pages/login.php
@@ -14,7 +14,7 @@
         <div class="text-center">
         <?php
         if($_SESSION['username']!='' && $page_path == 'pages/login'){
-            header('Location: /servers');
+            header('Location: '. $GET_["continue"] . '');
             die();
         }
         if(isset($_POST['Submit'])){
@@ -61,7 +61,7 @@
                 $_SESSION['username'] = $usernamesql;
                 $_SESSION['rank'] = $rank;
                 print_r($_SESSION);
-                header('Location: /');
+                header('Location: '. $GET_["continue"] . '');
                 die();
             } else {
                 echo 'Invalid username/password.';
