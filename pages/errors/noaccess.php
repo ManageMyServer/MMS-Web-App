51a919ba3a65b7808d20a3b75d5889bec56c3cd9
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-27 08:59:10 -0500

    Create noaccess.php

---
+++ pages/errors/noaccess.php
@@ -0,0 +1,8 @@
+<div class="text-center">
+    <h2>No Permission</h2>
+    <h5>You do not have permission to access this page.</h5>
+    <div class="btn-group" role="group" aria-label="...">
+        <a href="#" class="btn btn-success btn-medium" onclick="window.history.back()">Back</a>
+        <a href="/" class="btn btn-primary btn-medium">Home</a>
+    </div>
+</div>
