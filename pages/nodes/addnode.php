baa728c7018d0387f84e13e679ac629d845f62c2
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-01-30 23:19:49 -0500

    Mock up (make functional)

--- pages/nodes/addnode.php
+++ pages/nodes/addnode.php
@@ -1,3 +1,23 @@
-<?php
-    echo 'Add node page.';
-?>
+<h2>Add Node</h2>
+<p>
+Confused? Click the link to see how to add a node: <a href="https://managemyserver.github.io/docs/addnode.html">Instructions on how to add node.</a><br>
+</p>
+<div class="form-group">
+   <label class="control-label">Name the node</label>
+   <input type="text" class="form-control" placeholder="Node Name (Ex, US-Node1)">
+</div>
+<div class="form-group">
+   <label class="control-label">Public IP of the node</label>
+   <input type="text" class="form-control" placeholder="12.34.567.89">
+</div>
+<div class="form-group">
+   <label class="control-label">Type the code displayed when application is launched.</label>
+   <input type="text" class="form-control" placeholder="ys73j93">
+</div>
+<div class="form-group">
+   <label class="control-label">Type the availible ports separated by commas. (No spaces!)</label>
+   <input type="text" class="form-control" placeholder="25565,25626,26738">
+</div>
+<div class="btn-group" role="group">
+   <a href="/nodes"><button type="button" class="btn btn-primary">Create</button></a>
+</div>
