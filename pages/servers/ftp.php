6dc99b8259c665425fc93383d0f5f7b74b5f3cf3
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-02-05 13:33:39 -0500

    Added mock up

---
+++ pages/servers/ftp.php
@@ -0,0 +1,43 @@
+<h2 class="text-center">FTP <small>The Vanilla Server</small></h2>
+<div class="row">
+<div class="col-xs-3">
+<div class="btn-group-vertical">
+   <a href="server" class="btn btn-raised btn-primary">Overview</a>
+   <a href="console" class="btn btn-raised btn-primary">Console</a>
+   <a href="ftp" class="btn btn-raised btn-primary">FTP</a>
+   <a href="mysql" class="btn btn-raised btn-primary">MySQL</a>
+   <a class="btn btn-raised"></a>
+   <a href="" class="btn btn-raised btn-warning">Suspend Server</a>
+   <a href="" class="btn btn-raised btn-danger">Delete Server</a>
+</div>
+</div>
+<div class="col-xs-9">
+
+<div class="btn-group btn-group-justified">
+   <a href="" class="btn btn-raised btn-primary">Start</a>
+   <a href="" class="btn btn-raised btn-primary">Stop</a>
+   <a href="" class="btn btn-raised btn-primary">Restart</a>
+   <a href="" class="btn btn-raised btn-primary">Kill</a>
+</div>
+<div class="form-group">
+   <input disabled type="text" class="form-control" placeholder="Server Status" value="Online (9/25)">
+
+   <label class="control-label">FTP Address</label>
+   <input disabled type="text" class="form-control" placeholder="FTP Address" value="127.0.0.1">
+
+   <label class="control-label">FTP Username</label>
+   <input disabled type="text" class="form-control" placeholder="FTP Username" value="server1">
+
+   <label class="control-label">FTP Password</label>
+   <div class="input-group">
+   <input disabled type="text" class="form-control" placeholder="FTP Password" value="g6sq9l0">
+   <span class="input-group-btn">
+       <button class="btn btn-raised btn-danger" type="button">Get New Password</button>
+   </span>
+   </div>
+</div>
+<div class="btn-group" role="group">
+   <a href=""><button type="button" class="btn btn-primary">Access WebFTP (Comming Soon)</button></a>
+</div>
+</div>
+</div>
