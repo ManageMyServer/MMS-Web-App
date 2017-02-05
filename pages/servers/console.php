3948365fa0c1917898cb0167f08aebede2699384
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-02-05 13:34:32 -0500

    Added mock up

---
+++ pages/servers/console.php
@@ -0,0 +1,41 @@
+<h2 class="text-center">Console <small>The Vanilla Server</small></h2>
+<div class="row">
+<div class="col-xs-3">
+<div class="btn-group-vertical">
+   <a href="server" class="btn btn-raised btn-primary">Overview</a>
+   <a href="console" class="btn btn-raised btn-primary">Console</a>
+   <a href="ftp" class="btn btn-raised btn-primary">FTP</a>
+   <a href="mysql" class="btn btn-raised btn-primary">MySQL</a>
+   <a class="btn btn-raised"></a>
+   <a href="" class="btn btn-raised btn-warning">Suspend Server</a>
+   <a href="" class="btn btn-raised btn-danger">Delete Server</a>
+</div>
+</div>
+<div class="col-xs-9">
+
+<div class="btn-group btn-group-justified">
+   <a href="" class="btn btn-raised btn-primary">Start</a>
+   <a href="" class="btn btn-raised btn-primary">Stop</a>
+   <a href="" class="btn btn-raised btn-primary">Restart</a>
+   <a href="" class="btn btn-raised btn-primary">Kill</a>
+</div>
+<div class="form-group">
+   
+   <input disabled type="text" class="form-control" placeholder="Server Status" value="Online (9/25)">
+</div>
+<div class="panel panel-default">
+   <div class="panel-body">
+      <br><br><br><br><br><br><br><br><br>
+   </div>
+</div>
+<div class="form-group">
+   <div class="input-group">
+      <span class="input-group-addon">/</span>
+      <input type="text" class="form-control">
+      <span class="input-group-btn">
+         <button class="btn btn-raised btn-primary" type="button">Send</button>
+      </span>
+   </div>
+</div>
+</div>
+</div>
