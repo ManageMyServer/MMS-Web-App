eadd165a13fa6ec70618ed09ce3641771533d377
BrightSkyz <BrightSkyz@users.noreply.github.com>
2017-02-05 20:42:46 -0500

    Finished Status Page

--- pages/status.php
+++ pages/status.php
@@ -1,4 +1,5 @@
-<span class="text-center"><h2>Status <small>Non-Functional (Work in Progress, Display only)</small></h2></span>
+<?php require_once($_SERVER['DOCUMENT_ROOT']."/core/classes/nodes.php"); $node = new nodes();?>
+<span class="text-center"><h2>Status</h2></span>
 <table class="table">
     <thead>
         <tr>
@@ -7,13 +8,18 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>Node1</td>
-            <td><span class="label label-success">Online</span></td>
-        </tr>
-        <tr>
-            <td>Node2</td>
-            <td><span class="label label-danger">Offline</span></td>
-        </tr>
+        <?php
+            $nodes = $node->getNodes();
+            foreach($nodes as $nodeid) {
+                echo'<tr><td>'.$node->getNodeName($nodeid).'</td>';
+                $status = $node->getNodeStatus($nodeid);
+                if($status) {
+                    echo'<td><span class="label label-success">Online</span></td></tr>';
+                } else {
+                    echo'<td><span class="label label-danger">Offline</span></td></tr>';
+                }
+
+            }
+        ?>
     </tbody>
 </table>
